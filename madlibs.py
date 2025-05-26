f3784bb97a81d11b4f8ef62bd9f14c09b0394ef1
Nasir1010 <nhashi44@gmail.com>
2025-05-25 20:27:36 -0500

    finalized working madlibs.py
    This is a beginner Madlibs game easy to understand if you are a beginner like me.

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+noun1 = input("enter a noun")
+verb1 = input("enter a verb")
+verb2 = input("enter a verb")
+noun2 = input("enter a noun")
+adjective1 = input("enter an adjective")
+verb3 = input("enter a verb")
+
+print(f"""
+story tiime! 
+before this week I thought {noun1} was hard. 
+I was already {verb1}, but I was {verb2}.
+Nnow that I found {noun2}, I feel more 
+{adjective1} that I can {verb3} it.""")
